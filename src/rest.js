59aa7b36fa610bb2817cbcebc709ddec5f7fba5c
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-07 15:59:21 +0300

    Refactored REST Server
    Created class for REST Server
    
    Added middleware for Person fetch

--- src/rest.js
+++ src/rest.js
@@ -7,36 +7,56 @@ var RabbitClient = require('./RabbitClient');
 const PORT = 8080;
 const DEFAULT_NAME = 'Anonymous';
 
-var settings = {
-    client: null
-};
+class RestServer {
+    constructor (rabbitClient) {
+        assert(rabbitClient instanceof RabbitClient);
+        this.rabbitClient = rabbitClient;
 
-function get (req, res) {
-    assert(typeof settings.client.rpc === 'function');
+        var app = express();
 
-    var name = req.query.name || DEFAULT_NAME;
-    settings.client.rpc(name, function (err, msg) {
-        if (err) {
-            console.error(` [e] failed with ${err}`);
-            res.send(err);
-            return;
-        }
-        console.log(` [.] Got ${msg.content.toString()}`);
-        res.send(msg.content.toString());
-    });
+        app.param('person_name', this.fetchPersonMiddleware);
+        app.get('/', this.getAnonymous);
+        app.get('/:person_name', this.getPerson);
+
+        app.listen(PORT);
+        console.log('Application is ready');
+    }
+    getPerson (req, res) {
+        res.json({
+            success: true,
+            response: req.greeting
+        });
+    }
+    getAnonymous (req, res) {
+        this.findPerson(DEFAULT_NAME, (err, result) => {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            res.json({
+                success: true,
+                response: result
+            });
+        });
+    }
+    fetchPersonMiddleware (request, response, next, person_name) {
+        this.findPerson(person_name, (err, result) => {
+            request.greeting = result;
+            return next();
+        });
+    }
+    findPerson (name, callback) {
+        this.rabbitClient.rpc(name, function (err, msg) {
+            if (err) {
+                console.error(` [e] failed with ${err}`);
+                return callback(err);
+            }
+            console.log(` [.] Got ${msg.content.toString()}`);
+            callback(null, msg.content.toString());
+        });
+    }
 }
 
-function init (client) {
-    assert(client instanceof RabbitClient);
-    settings.client = client;
-
-    var app = express();
-
-    app.get('/', get);
-
-    app.listen(PORT);
-    console.log('Application is ready');
-}
-
-module.exports = init;
+//module.exports = init;
+module.exports = RestServer;
 
